323b36c3242439e8692e11cd559e0973229a06d5
accaria <anugerahbarraqi1997@gmail.com>
2026-03-02 16:44:19 +0700

    feat: more basic syntaxes

--- main.go
+++ main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+var job string = "Mechanic"
+
 func main() {
+	var name string = "Aria"
+	var age int = 30
+	name2 := "Liam"
+	age2 := 25
+	fmt.Println("Hello, my name is", name, "and I am", age, "years old. I works as a", job+".")
+	fmt.Println("Hello, my name is", name2, "and I am", age2, "years old.")
 	fmt.Println("Hello, World!")
+
+	if age > age2 {
+		fmt.Println("Hey, I'm", age, "years old")
+	}
+	if score := 90; score >= 75 {
+		fmt.Println("Passed")
+	}
+
+	for ia := 0; ia < 5; ia++ {
+		fmt.Println(ia)
+	}
+
+	count := 0
+	for count < 5 {
+		fmt.Println(count)
+		count++
+	}
+
 }
